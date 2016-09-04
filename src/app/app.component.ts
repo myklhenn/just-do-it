7229e9eb1307269ff628b13e4271fc09edc0ff3a
Michael Henning <myklhenn@icloud.com>
2016-09-03 17:30:33 -0700

    navbar component init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import '../../public/less/styles.less';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
   
-export class AppComponent { }
+export class AppComponent { 
+  
+  title: string = "Hello World";
+}
